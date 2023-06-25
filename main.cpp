cd6ea540801e06f78232b19b848f7f48907a88b5
Shivansh Bhardwaj <96634814+bhardwajshivansh@users.noreply.github.com>
2023-06-26 02:28:17 +0530

    made intial classes and basic body

---
+++ main.cpp
@@ -0,0 +1,43 @@
+//trvael management sysytem
+#include<iostream>
+#include<fstream>//this is to handle our files
+#include<iomanip>//use to show floating point values
+#include<windows.h>
+
+using namespace std;
+//creating various classes according to the need
+
+//customers class
+class Customers{
+
+    public:
+    //creating various attributes
+    string name, gender,address;
+    int age, mobileNumber,customerid;
+
+};
+
+//Cabs class
+class Cabs{
+
+};
+
+
+//Booking class
+class Booking{
+
+};
+
+//Charges class
+class Charges{
+
+};
+
+
+
+//main function
+int main(){
+
+    cout<<"hello world";
+    return 0;
+}
